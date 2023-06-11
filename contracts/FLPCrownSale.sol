4898afd3e62af638811fd7dc3fb58754feddffb8
vanhung218 <hungdv2182002@gmail.com>
2023-06-11 14:25:32 +0700

    fd

--- contracts/FLPCrownSale.sol
+++ contracts/FLPCrownSale.sol
@@ -1,101 +1,164 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity <=0.8.10;
 
+import "openzeppelin-solidity/contracts/token/ERC721/IERC721Receiver.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
 import "openzeppelin-solidity/contracts/access/Ownable.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract FLPCrownSale is Ownable {
+contract HeroMarketplace is IERC721Receiver, Ownable {
     using SafeERC20 for IERC20;
-    address payable public _wallet;
-    uint256 public BNB_rate;
-    uint256 public USDT_rate;
-    IERC20 public token;
-    IERC20 public usdtToken;
+    IERC721Enumerable private nft;
+    IERC20 private token;
 
-    event BuyTokenByBNB(address buyer, uint256 amount);
-    event BuyTokenByUSDT(address buyer, uint256 amount);
-    event SetUSDTToken(IERC20 tokenAddress);
-    event SetBNBRate(uint256 newRate);
-    event SetUSDTRate(uint256 newRate);
-
-    constructor(
-        uint256 bnb_rate,
-        uint256 usdt_rate,
-        address payable wallet,
-        IERC20 icotoken
-    ) public {
-        BNB_rate = bnb_rate;
-        USDT_rate = usdt_rate;
-        _wallet = wallet;
-        token = icotoken;
+    struct ListDetail {
+        address payable author;
+        uint256 price;
+        uint256 tokenId;
     }
 
-    function setUSDTToken(IERC20 token_address) public onlyOwner {
-        usdtToken = token_address;
-        emit SetUSDTToken(token_address);
+    event ListNFT(address indexed _from, uint256 _tokenId, uint256 _price);
+    event UnlistNFT(address indexed _from, uint256 _tokenId);
+    event BuyNFT(address indexed _from, uint256 _tokenId, uint256 _price);
+    event UpdateListingNFTPrice(uint256 _tokenId, uint256 _price);
+    event SetToken(IERC20 _token);
+    event SetTax(uint256 _tax);
+
+    event SetNFT(IERC721Enumerable _nft);
+
+    uint256 private tax = 10; // percentage
+    mapping(uint256 => ListDetail) listDetail;
+
+    constructor(IERC20 _token, IERC721Enumerable _nft) {
+        nft = _nft;
+        token = _token;
     }
 
-    function setBNBRate(uint256 new_rate) public onlyOwner {
-        BNB_rate = new_rate;
-        emit SetBNBRate(new_rate);
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes calldata
+    ) external pure override returns (bytes4) {
+        return
+            bytes4(
+                keccak256("onERC721Received(address,address,uint256,bytes)")
+            );
     }
 
-    function setUSDTRate(uint256 new_rate) public onlyOwner {
-        USDT_rate = new_rate;
-        emit SetUSDTRate(new_rate);
+    function setTax(uint256 _tax) public onlyOwner {
+        tax = _tax;
+        emit SetTax(_tax);
     }
 
-    function buyTokenByBNB() external payable {
-        uint256 bnbAmount = msg.value;
-        uint256 amount = getTokenAmountBNB(bnbAmount);
-        require(amount > 0, "Amount is zero");
+    function setToken(IERC20 _token) public onlyOwner {
+        token = _token;
+        emit SetToken(_token);
+    }
+
+    function setNft(IERC721Enumerable _nft) public onlyOwner {
+        nft = _nft;
+        emit SetNFT(_nft);
+    }
+
+    function getListedNft() public view returns (ListDetail[] memory) {
+        uint balance = nft.balanceOf(address(this));
+        ListDetail[] memory myNft = new ListDetail[](balance);
+
+        for (uint i = 0; i < balance; i++) {
+            myNft[i] = listDetail[nft.tokenOfOwnerByIndex(address(this), i)];
+        }
+        return myNft;
+    }
+
+    function listNft(uint256 _tokenId, uint256 _price) public {
         require(
-            token.balanceOf(address(this)) >= amount,
+            nft.ownerOf(_tokenId) == msg.sender,
+            "You are not the owner of this NFT"
+        );
+        require(
+            nft.getApproved(_tokenId) == address(this),
+            "Marketplace is not approved to transfer this NFT"
+        );
+
+        listDetail[_tokenId] = ListDetail(
+            payable(msg.sender),
+            _price,
+            _tokenId
+        );
+
+        nft.safeTransferFrom(msg.sender, address(this), _tokenId);
+        emit ListNFT(msg.sender, _tokenId, _price);
+    }
+
+    function updateListingNftPrice(uint256 _tokenId, uint256 _price) public {
+        require(
+            nft.ownerOf(_tokenId) == address(this),
+            "This NFT doesn't exist on marketplace"
+        );
+        require(
+            listDetail[_tokenId].author == msg.sender,
+            "Only owner can update price of this NFT"
+        );
+
+        listDetail[_tokenId].price = _price;
+        emit UpdateListingNFTPrice(_tokenId, _price);
+    }
+
+    function unlistNft(uint256 _tokenId) public {
+        require(
+            nft.ownerOf(_tokenId) == address(this),
+            "This NFT doesn't exist on marketplace"
+        );
+        require(
+            listDetail[_tokenId].author == msg.sender,
+            "Only owner can unlist this NFT"
+        );
+
+        nft.safeTransferFrom(address(this), msg.sender, _tokenId);
+        emit UnlistNFT(msg.sender, _tokenId);
+    }
+
+    function buyNft(uint256 _tokenId, uint256 _price) public {
+        require(
+            token.balanceOf(msg.sender) >= _price,
             "Insufficient account balance"
         );
         require(
-            msg.sender.balance >= bnbAmount,
-            "Insufficient account balance"
+            nft.ownerOf(_tokenId) == address(this),
+            "This NFT doesn't exist on marketplace"
         );
-        payable(_wallet).transfer(bnbAmount);
-        SafeERC20.safeTransfer(token, msg.sender, amount);
-        emit BuyTokenByBNB(msg.sender, amount);
-    }
-
-    function buyTokenByUSDT(uint256 USDTAmount) external {
-        uint256 amount = getTokenAmountUSDT(USDTAmount);
         require(
-            msg.sender.balance >= USDTAmount,
-            "Insufficient account balance"
+            listDetail[_tokenId].price <= _price,
+            "Minimum price has not been reached"
         );
-        require(amount > 0, "Amount is zero");
-        require(
-            token.balanceOf(address(this)) >= amount,
-            "Insufficient account balance"
+
+        SafeERC20.safeTransferFrom(token, msg.sender, address(this), _price);
+        token.transfer(
+            listDetail[_tokenId].author,
+            (_price * (100 - tax)) / 100
         );
-        SafeERC20.safeTransferFrom(usdtToken, msg.sender, _wallet, USDTAmount);
-        SafeERC20.safeTransfer(token, msg.sender, amount);
-        emit BuyTokenByUSDT(msg.sender, amount);
+
+        nft.safeTransferFrom(address(this), msg.sender, _tokenId);
+        emit BuyNFT(msg.sender, _tokenId, _price);
     }
 
-    function getTokenAmountBNB(
-        uint256 BNBAmount
-    ) public view returns (uint256) {
-        return BNBAmount * BNB_rate;
-    }
-
-    function getTokenAmountUSDT(
-        uint256 USDTAmount
-    ) public view returns (uint256) {
-        return USDTAmount * USDT_rate;
-    }
+    //
 
     function withdraw() public onlyOwner {
         payable(msg.sender).transfer(address(this).balance);
     }
 
+    function withdrawToken(uint256 amount) public onlyOwner {
+        require(
+            token.balanceOf(address(this)) >= amount,
+            "Insufficient account balance"
+        );
+        token.transfer(msg.sender, amount);
+    }
+
     function withdrawErc20() public onlyOwner {
-        usdtToken.transfer(msg.sender, usdtToken.balanceOf(address(this)));
+        token.transfer(msg.sender, token.balanceOf(address(this)));
     }
 }
